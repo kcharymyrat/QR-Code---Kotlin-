7b6e57bde96596562bb7319ad2bc7d91ccd1d65c
Charymyrat Garryyev <kcharymyrat@gmail.com>
2024-05-19 14:32:36 +0500

    Stage 3 done

--- QRCode Service (Kotlin)/task/src/qrcodeapi/controller/QRCodeRestController.kt
+++ QRCode Service (Kotlin)/task/src/qrcodeapi/controller/QRCodeRestController.kt
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import qrcodeapi.custom_error.CustomError
 import qrcodeapi.services.QRCodeImageGeneratorService
 import java.awt.image.BufferedImage
 
@@ -19,12 +21,33 @@ class QRCodeRestController @Autowired constructor(
     fun getHealth() = ResponseEntity("Hello World", HttpStatus.OK);
 
     @GetMapping("/api/qrcode")
-    fun getQRCode(): ResponseEntity<BufferedImage> {
+    fun getQRCodeImage(
+        @RequestParam(value="size", defaultValue = "250") size: Int,
+        @RequestParam(value="type", defaultValue = "png") type: String
+    ): ResponseEntity<out Any>? {
+
+        if (size < 150 || size > 350) {
+            return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(CustomError("Image size must be between 150 and 350 pixels"))
+        }
+        val mediaType = if (type.trim { it <= ' ' }.equals("png", ignoreCase = true)) {
+            MediaType.IMAGE_PNG
+        } else if (type.trim { it <= ' ' }.equals("jpeg", ignoreCase = true)) {
+            MediaType.IMAGE_JPEG
+        } else if (type.trim { it <= ' ' }.equals("gif", ignoreCase = true)) {
+            MediaType.IMAGE_GIF
+        } else {
+            return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body<Any>(CustomError("Only png, jpeg and gif image types are supported"))
+        }
+
         val bufferedImage: BufferedImage = qrCodeImageGeneratorService
-            .generateQRCOdeBufferedImage(250, 250)
+            .generateQRCOdeBufferedImage(size, size)
         return ResponseEntity
             .ok()
-            .contentType(MediaType.IMAGE_PNG)
+            .contentType(mediaType)
             .body(bufferedImage)
     }
 
---
+++ QRCode Service (Kotlin)/task/src/qrcodeapi/custom_error/CustomError.kt
@@ -0,0 +1,3 @@
+package qrcodeapi.custom_error
+
+data class CustomError(val error: String)
